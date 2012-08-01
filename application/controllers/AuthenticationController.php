d6f8ee6209f32f45b29d25ac9d0fe9cc0f80a6dc
Avi Yacoby <yacobya@zahav.net.il>
2012-08-01 23:06:17 +0300

    correcting login sha1 treatment

--- application/controllers/AuthenticationController.php
+++ application/controllers/AuthenticationController.php
@@ -16,46 +16,73 @@ class AuthenticationController extends Zend_Controller_Action
 
     public function loginAction()
     {
+        	
+    	// check if user is already logged in
     	if (Zend_Auth::getInstance()->hasIdentity()){
-    		$this->_redirect('index/index');
+    		$this->_redirect('index/index'); // user is logged in
     	}
-       // create Login form
-        $form= new Application_Form_Login();
-        $this->view->form = $form;
+
+    	//user is not logged yet
     	// check users DB content, if empty create user "admin"
     	$usersDb = new Application_Model_DbTable_Users();
     	$usersDb->initUsers();// check if empty--> create admin default user
-		// test authoentication
-        $username='wrong';
-        $userPassword = Application_Model_DbTable_Users::PASSWORD_SALT . 'admin';//adding constant salt to users password
-        $authAdapter=$this->getAuthAdapter();
-        $authAdapter->setIdentity($username)
-                    ->setCredential($userPassword);// need to encode password
-        $auth = Zend_Auth::getInstance();
-        $result=$auth->authenticate($authAdapter);
-        if ($result->isValid()){
-        	 $userAttrib = $authAdapter->getResultRowObject();
-        	 $authStorage = $auth->getStorage();
-        	 $authStorage->write($userAttrib);
-        	 $this->_redirect('index/index');       	 
-        	
-        } 
-        else {
-        	;
-        }
-                      
+
+    	// determine if POST perfromed
+    	$form= new Application_Form_Login();
+
+    	if ($this->getRequest()->isPost() ){
+    		if ($form->isValid($this->_request->getPost())){
+
+    			//user tried to login. Check authoentication
+    			// read assign username and password
+    			$username = $form->getValue('username');
+    			$password = $form->getValue('password');
+    			//add password salt
+    			$password = Application_Model_DbTable_Users::PASSWORD_SALT . $password;
+    			
+    			$authAdapter=$this->getAuthAdapter();
+    			$authAdapter->setIdentity($username)
+    			->setCredential($password);
+    			$auth = Zend_Auth::getInstance();
+    			$result=$auth->authenticate($authAdapter);
+    			
+    			if ($result->isValid()){
+    				// user is authorized
+    				$userAttrib = $authAdapter->getResultRowObject();
+    				$authStorage = $auth->getStorage();
+    				$authStorage->write($userAttrib);
+    				$this->_redirect('index/index');
+    			}else{ //user not authorized - return to login form
+    				// set error message
+    	 		 	$this->view->errorMsg = 'illgal password or username<br>';
+    				$this->view->form = $form;
+    			}
+    		}
+    		else {// invalid form data
+    			$this->view->errorMsg = 'illgal form data';
+    			$this->view->form = $form;
+    		}
+    	}
+    	else {// request not posted - render login form
+     			$this->view->form = $form;
+    	}
+    		
+		
     }
 
     public function logoutAction()
     {
-        // action body
+    	Zend_Auth::getInstance()->clearIdentity();
+    	$this->_redirect('index/index');
+
     }
+    
     private function getAuthAdapter(){
     	$authAdapter = new Zend_Auth_Adapter_DbTable(Zend_Db_Table::getDefaultAdapter());
     	$authAdapter->setTableName('cc_users')
     				->setIdentityColumn('username')
     				->setCredentialColumn('password')
-    				->setCredentialTreatment('SHA1(?)');
+    				->setCredentialTreatment('sha1(?)');
     	// 
     	return $authAdapter;
     				
