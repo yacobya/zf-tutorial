1078455967c6077b4f5d975ecc92e3cb4f2b6715
Avi Yacoby <yacobya@zahav.net.il>
2012-08-14 13:14:02 +0300

    many bugs, need to be recover. temporary storage

--- application/controllers/UsersManagementController.php
+++ application/controllers/UsersManagementController.php
@@ -5,7 +5,11 @@ class UsersManagementController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        /* Initialize context switch to manage AJAX requests*/
+    	$ajaxContext = $this->_helper->getHelper('AjaxContext');
+        $ajaxContext->addActionContext('add', 'json')
+                     ->initContext();
+    	
     }
 
     public function indexAction()
@@ -19,34 +23,57 @@ class UsersManagementController extends Zend_Controller_Action
 
     public function addAction()
     {
-        $form=new Application_Form_User();
-    	$form->submit->setLabel('Add');
-    	$this->view->form = $form;
-    	$validData = false;
-    	if ($this->getRequest()->isPost()) {
-    		$formData = $this->getRequest()->getPost();
-    		if ($form->isValid($formData)) {
-    			$username = $form->getValue('username');
-    			$userRole = $form->getValue('userRole');
-    			$password = $form->getValue('password');
-    			$passwordValid = $form->getValue('passwordValid');
-    			// Check password confirmation
-    			if (!strcmp($password, $passwordValid)){
-    				// all data is valid - insert new user to DB
-    				$users = new Application_Model_DbTable_Users();
-    				$users->addUser($username, $password,$userRole);
-    				$validData=true;
-    				$this->_helper->redirector('index');
-    			
-    			}
-    				
-    		} 
-    		// check data valid
-    		if (!$validData){
-     			$this->view->errorMsg="<br>Password confirmation error<br>";
-  				$form->populate($formData);
-    		}
-    	}// end isPost
+
+    	//Check if Ajax request
+    	$usersDb=null;	 
+    	$formData = $this->getRequest()->getPost();
+    	// check if ajax request - check user existance
+    	if ('json' == $this->_getParam('format', false))
+    	{
+    		$username=$this->_getParam('username',false);
+    		$usersDb = new Application_Model_DbTable_Users();
+    		$user=$usersDb->getUser('username',$username);
+    		if ($user!=null)
+    			// set user exist
+    			$userExist=true;
+    		else
+    			$userExist=false;
+    		$this->view->userExist=$userExist;  		
+    	}
+    	else
+    	{
+		    	 
+	    	$form=new Application_Form_User();
+	//    	$form->submit->setLabel('Add');
+	    	$this->view->form = $form;
+	    	$validData = false;
+	    	$formData = $this->getRequest()->getPost();
+	    	
+	    	if ($this->getRequest()->isPost()) {
+	    		$formData = $this->getRequest()->getPost();
+	    		if ($form->isValid($formData)) {
+	    			$username = $form->getValue('username');
+	    			$userRole = $form->getValue('userRole');
+	    			$password = $form->getValue('password');
+	    			$passwordValid = $form->getValue('passwordValid');
+	    			// Check password confirmation
+	    			if (!strcmp($password, $passwordValid)){
+	    				// all data is valid - insert new user to DB
+	    				$users = new Application_Model_DbTable_Users();
+	    				$users->addUser($username, $password,$userRole);
+	    				$validData=true;
+	    				$this->_helper->redirector('index');
+	    			
+	    			}
+	    				
+	    		} 
+	    		// check data valid
+	    		if (!$validData){
+	     			$this->view->errorMsg="<br>Password confirmation error<br>";
+	  				$form->populate($formData);
+	    		}
+	    	}// end isPost
+    	}
     }
     public function deleteAction()
     {
@@ -66,16 +93,17 @@ class UsersManagementController extends Zend_Controller_Action
     		
     	}
     }
-
-    public function editAction()
-    {                               
-        // action body
+    
+ public function editAction()
+    {
+    	// action body
     }
-
+    
     public function listAction()
     {
         // action body
     }
+    
 
 
 }
