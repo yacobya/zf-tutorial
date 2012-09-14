e8de3b097af67d6fc64b9401c55003da27a85b95
Avi Yacoby <yacobya@zahav.net.il>
2012-09-14 12:24:33 +0300

    include array of contenet headers

---
+++ application/configs/contentHeader.php
@@ -0,0 +1,7 @@
+<?php
+$contentHeader=array('index'=>array('index'=>'Status'),
+					 'usersmanagement'=>array('index'=>'Users List','add'=>'Add User',
+					 		'delete'=>'Users List'),
+					 'authentication'=>array('login'=>'Login'),
+					 'config'=>array('listsources'=>'Video Sources','index'=>'Video Sources','addsource'=>'Add / Delete Sources')
+		       );
